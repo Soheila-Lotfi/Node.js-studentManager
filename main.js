29f78e00902caf2ab67662fae55e0e2fc0abf094
soheila <soheilalotfi@ymail.com>
2019-07-29 16:32:48 -0400

    add main.js-create a classroom with two students

---
+++ main.js
@@ -0,0 +1,8 @@
+var Classroom = require("./classroom.js");
+
+var firstClass = new Classroom("Adam", 208);
+
+firstClass.addstudents("soheila", "javascript", "A");
+firstClass.addstudents("Farzad", "React.js", "A");
+
+console.log(firstClass);
