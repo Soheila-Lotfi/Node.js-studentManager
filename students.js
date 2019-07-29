bf189e73ca9a78b34e4ed15054dc7d3335d534fc
soheila <soheilalotfi@ymail.com>
2019-07-29 15:13:24 -0400

    create student constuctor

---
+++ students.js
@@ -0,0 +1,8 @@
+// constructor function for creating student object
+var Student = function(name, favoritSub, gpa) {
+  this.name = name;
+  this.favoritSub = favoritSub;
+  this.gpa = gpa;
+};
+
+module.exports = Student;
